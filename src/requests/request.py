aee23237768c32b633d8614662394ffbc825bc8a
Frengocode <164409199+Frengocode@users.noreply.github.com>
2024-10-15 17:34:37 +0500

    Update request.py

--- src/requests/request.py
+++ src/requests/request.py
@@ -1,5 +1,5 @@
 
-AUTH_LOGIN = "http://your_server_host/auth/login/"
-GET_USER_REQUEST = "http://your_server_host/auth/get-user"
-GET_VIDEO_REQUEST = "http://localhost:8000/video-service/get-video"
-GET_CURRENT_USER_REQUEST = "http://your_server_host/auth/get-current-user"
+AUTH_LOGIN = "http://your_server_host/auth/login/"  # Where Host Running Video-Auth-Service  
+GET_USER_REQUEST = "http://your_server_host/auth/get-user" # Where Host Running Video-Auth-Service
+GET_VIDEO_REQUEST = "http://localhost:8000/video-service/get-video" 
+GET_CURRENT_USER_REQUEST = "http://your_server_host/auth/get-current-user" # Where Host Running Video-Auth-Service
