05f8974532677748f319287be2a9fe6ddb565f0f
Frengocode <164409199+Frengocode@users.noreply.github.com>
2024-10-15 17:40:28 +0500

    Update database.py

--- src/services/comment_service/database.py
+++ src/services/comment_service/database.py
@@ -7,7 +7,7 @@ import logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-DATABASE_URL = "postgresql+asyncpg://postgres:python$_venv@localhost:5432/CommetBase"
+DATABASE_URL = "postgresql+asyncpg://username:password@localhost:5432/CommetBase"
 
 CommentBase = declarative_base()
 
