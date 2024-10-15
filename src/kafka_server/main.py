55cbac0f3c231ecc30e83f3f978ceb95312327f3
Frengocode <164409199+Frengocode@users.noreply.github.com>
2024-10-15 17:36:06 +0500

    Update main.py

--- src/kafka_server/main.py
+++ src/kafka_server/main.py
@@ -2,10 +2,10 @@ import asyncio
 from aiokafka import AIOKafkaProducer
 
 async def get_kafka_producer() -> AIOKafkaProducer:
-    loop = asyncio.get_event_loop()  # Получаем текущий event loop
+    loop = asyncio.get_event_loop()  
     producer = AIOKafkaProducer(
-        loop=loop,  # Явно передаем event loop
-        bootstrap_servers="localhost:9092"  # Укажи свой адрес Kafka сервера
+        loop=loop,  #
+        bootstrap_servers="localhost:9092"  # Paste Your Host Where host working your kafka server
     )
     await producer.start()
     try:
