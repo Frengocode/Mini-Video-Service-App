4f0d2a81746fe3f79b6bc7e913c315914399e84f
Frengocode <164409199+Frengocode@users.noreply.github.com>
2024-10-15 17:33:14 +0500

    Update request.py

--- src/requests/request.py
+++ src/requests/request.py
@@ -1,5 +1,5 @@
 
-AUTH_LOGIN = "http://192.168.100.230:8000/auth/login/"
-GET_USER_REQUEST = "http://192.168.100.230:8000/auth/get-user"
+AUTH_LOGIN = "http://your_server_host/auth/login/"
+GET_USER_REQUEST = "http://your_server_host/auth/get-user"
 GET_VIDEO_REQUEST = "http://localhost:8000/video-service/get-video"
-GET_CURRENT_USER_REQUEST = "http://192.168.100.230/auth/get-current-user"
+GET_CURRENT_USER_REQUEST = "http://your_server_host/auth/get-current-user"
